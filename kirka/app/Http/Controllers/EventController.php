cca8876e23afb54832a17921cfc38f4e2446e172
kametazin <0x0e0w@gmail.com>
2024-03-19 20:40:21 +0100

    feat: add controllers

--- kirka/app/Http/Controllers/EventController.php
+++ kirka/app/Http/Controllers/EventController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
-    //
+    public function submit(Request $req) {
+        $event = new Event();
+        $event->event_name = $req->input('event_name');
+        $event->event_date = $req->input('event_date');
+        $event->description = $req->input('description');
+        $event->checkin = $req->input('checkin');
+
+        $event->save();
+
+        //return redirect()->route('home');
+        //если заполнение не работает сбросить кэш: php artisan config:cache
+    }
 }
--- kirka/app/Http/Controllers/UserController.php
+++ kirka/app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function submit(Request $req) {
+        $user = new User();
+        $user->name = $req->input('name');
+        $user->email = $req->input('email');
+        $user->password = $req->input('password');
+
+        $user->save();
+
+        //return redirect()->route('home');
+        //если заполнение не работает сбросить кэш: php artisan config:cache
+    }
 }
