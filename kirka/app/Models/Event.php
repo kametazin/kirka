77cea3b0dee5ac25df111de05ed531fe62bb1889
kametazin <0x0e0w@gmail.com>
2024-03-19 20:44:18 +0100

    fix: event model

--- kirka/app/Models/Event.php
+++ kirka/app/Models/Event.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'event_name',
+        'event_date',
+        'description',
+        'checkin',
+    ];
 }
